27cbd2d27e3f32558dafba985cfa568dbabfd0bf
Fabian <ramirezfabian003@gmail.com>
2020-06-07 16:05:13 -0600

    Toda la estructura

---
+++ cmake-build-debug/CMakeFiles/Pole_Position_Server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pole_Position_Server.dir/main.c.obj"
+  "Pole_Position_Server.exe"
+  "Pole_Position_Server.exe.manifest"
+  "Pole_Position_Server.pdb"
+  "libPole_Position_Server.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Pole_Position_Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Pole_Position_Server.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Pole_Position_Server.
+# This may be replaced when dependencies are built.
