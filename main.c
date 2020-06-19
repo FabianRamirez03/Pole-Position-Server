e68d14c3d38dfb3ff610e7b68f69bde9679a09d6
Fabian <ramirezfabian003@gmail.com>
2020-06-19 16:28:56 -0600

    Codigo comentado y finalizado

--- main.c
+++ main.c
@@ -8,41 +8,41 @@
 #define BUZZ_SIZE 1024
 
 
-// our thread for receiving commands
+// El hilo que recibe informacion
 DWORD WINAPI receive_cmds(LPVOID lpParam)
 
 
 {
-// set our socket to the socket passed in as a parameter
+// Iguala nuestro socket al socket enviado como parametro
     SOCKET current_client = (SOCKET)lpParam;
 
 
-    // buffer to hold our recived data
+    // buffer que contiene la informacion recibida
     char buf[100];
-    // buffer to hold our sent data
+    // buffer que contiene la informacion a enviar
     char sendData[100];
-    // for error checking
+    // revisa cualquier error producido
     int res;
-    int responseInt;
-    char response[100];
-    //Listas de informacion de cada vehiculo
 
-    //[name, posX, posY, scaleX, scaleY, speed]
-    char carBlueInfo[6][20];
-
-
+    //Archivo donde guardara la informacion recibida que debe ser sobreescrita
     FILE *fileToWrite;
+    //Archio donde leera la informacion para devolversela al cliente
     FILE *fileToRead;
 
+    //Si obtiene informacion del carro rojo, sobreescribe lo que obtuvo en el archivo
+    // del carro rojo y devuelve la informacion del carro azul para que actualice.
+    //En caso de ser el carro azul se haria al contrario
 
 
 
-    // our recv loop
 
     res = recv(current_client,buf,sizeof(buf),0); // recv cmds
+    //Copia de la informacion recibida por el cliente
     char bufCopy[50];
+    //copia la informacion recibida
     strcpy(bufCopy, buf);
 
+    //imprime que dijo el cliente para mayor claridad
     printf ("Client says: %s\n", buf);
 
     //Define cual de los dos carros envió informacion y la almacena en una lista
@@ -54,41 +54,44 @@ DWORD WINAPI receive_cmds(LPVOID lpParam)
     //En caso de que la informacion recibida sea del carro rojo
     result = strcmp(token, "red");
     if(result == 0){
-        fileToWrite = fopen("infoRedCar.txt","w");
-        fprintf(fileToWrite, "%s", buf);
-        fclose(fileToWrite);
+        fileToWrite = fopen("infoRedCar.txt","w"); //Asigna el arhivo a sobrescribir como el del carro rojo
+        fprintf(fileToWrite, "%s", buf); //Sobreescribe el archivo
+        fclose(fileToWrite); //Cierra el archivo
         char buff[BUZZ_SIZE];
-        fileToRead = fopen("infoBlueCar.txt", "r");
-        fgets(buff, BUZZ_SIZE, fileToRead);
-        strcpy(sendData, strcat(buff, "\n"));
-        fclose(fileToRead);
+        fileToRead = fopen("infoBlueCar.txt", "r"); //Asigna el archivo a leer como el del carro azul
+        fgets(buff, BUZZ_SIZE, fileToRead); //Obtiene la informacion
+        strcpy(sendData, strcat(buff, "\n")); //copia la informacion a sendData para ser enviada
+        fclose(fileToRead); //Cierra el archivo
     }
     //en caso de que la informacion ingresada sea del carro azul
     result = strcmp(token, "blue");
     if(result == 0){
-        fileToWrite = fopen("infoBlueCar.txt","w");
-        fprintf(fileToWrite, "%s", buf);
-        fclose(fileToWrite);
+        fileToWrite = fopen("infoBlueCar.txt","w");//Asigna el arhivo a sobrescribir como el del carro azul
+        fprintf(fileToWrite, "%s", buf);//Sobreescribe el archivo
+        fclose(fileToWrite);//Cierra el archivo
         char buff[BUZZ_SIZE];
-        fileToRead = fopen("infoRedCar.txt", "r");
-        fgets(buff, BUZZ_SIZE, fileToRead);
-        strcpy(sendData, strcat(buff, "\n"));
-        fclose(fileToRead);
+        fileToRead = fopen("infoRedCar.txt", "r");//Asigna el archivo a leer como el del carro rojo
+        fgets(buff, BUZZ_SIZE, fileToRead);//Obtiene la informacion
+        strcpy(sendData, strcat(buff, "\n"));//copia la informacion a sendData para ser enviada
+        fclose(fileToRead);//Cierra el archivo
     }
-    result = strcmp(token, "bye");
+    //Vacia la informacion en los archivos cada vez que se cierra el programa para
+    //que no afecten partidas futuras
+
+    result = strcmp(token, "bye"); //Verifica si el programa debe ser cerrado
     if(result == 0){
-        printf("Terminado\n");
-        fileToWrite = fopen("infoBlueCar.txt","w");
-        fprintf(fileToWrite, "%s", "wait\n");
-        fclose(fileToWrite);
-        fileToRead = fopen("infoRedCar.txt","w");
+        printf("Terminado\n"); //Informa que se ha terminada la partida en consola
+        fileToWrite = fopen("infoBlueCar.txt","w");//Abre el archivo del carro rojo
+        fprintf(fileToWrite, "%s", "wait\n"); //Sobreescribe el archivo
+        fclose(fileToWrite);//Cierra el archivo
+        fileToRead = fopen("infoRedCar.txt","w"); //Procede a hacer lo mismo con el carro azul
         fprintf(fileToWrite, "%s", "wait\n");
         fclose(fileToRead);
     }
 
-    isReady = strcmp(sendData, "\0");
+    isReady = strcmp(sendData, "\0"); //Si no recibe nada quiere decir que alguno de los dos carros no esta listo
     if(isReady == 0){
-        strcpy(sendData, "wait\n");
+        strcpy(sendData, "wait\n"); //Devuelve wait para indicar que debe esperar a que se conecte el otro carro
     }
 
 
@@ -101,13 +104,16 @@ DWORD WINAPI receive_cmds(LPVOID lpParam)
     }
 
 
-    //strcpy(sendData,"Soy el server\n");
+    //Despues de manipular la informacion recibida y decidir que debe enviar
+
+    //Print en consola para la claridad de que sera la informacion que se va a enviar
     printf ("Server says: %s\n", sendData); //lo qhe dice el server
     Sleep(10);
+   //Envia la informacion al cliente
     send(current_client,sendData,sizeof(sendData),0);
 
 
-    // clear buffers
+    // Limpia ambos buffers
     strcpy(sendData,"");
     strcpy(buf,"");
 
@@ -117,69 +123,64 @@ DWORD WINAPI receive_cmds(LPVOID lpParam)
 
 int main() {
 
-    // our masterSocket(socket that listens for connections)
+    // Socket principal que recibira informacion
     SOCKET sock;
 
-    // for our thread
+    // Hilo donde se correra el servidor
     DWORD thread;
 
     WSADATA wsaData;
     struct sockaddr_in server;
 
-    // start winsock
-    int ret = WSAStartup(0x101,&wsaData); // use highest version of winsock available
+    // Inicia un winsock
+    int ret = WSAStartup(0x101,&wsaData); // utiliza la version mas reciente de winSocks
 
     if(ret != 0)
     {
         return 0;
     }
 
-    // fill in winsock struct ...
+    // Completa la estructura del WinSock
     server.sin_family=AF_INET;
     server.sin_addr.s_addr=INADDR_ANY;
     server.sin_port=htons(25557);
 
-    // create our socket
+    // Crea el soccket correctamente
     sock=socket(AF_INET,SOCK_STREAM,0);
 
+    // Si el socket fuera invalido, se detiene el programa
     if(sock == INVALID_SOCKET)
     {
         return 0;
     }
 
-    // bind our socket to a port
+    // se asegura que pueda asignar el socket al puerto identificado anteriormente. 25557 en este caso
     if( bind(sock,(struct sockaddr*)&server,sizeof(server)) !=0 )
     {
         return 0;
     }
 
-    // listen for a connection
+    // Escucha por conexiones
     if(listen(sock,5) != 0)
     {
         return 0;
     }
 
-    // socket that we snedzrecv data on
+    // socket encargado de enviar informacion
     SOCKET client;
 
     struct sockaddr_in from;
     int fromlen = sizeof(from);
 
-    // loop forever
+    // Ciclo que se mantiene corriendo durante la ejecucion del programa
     while(true)
     {
-        // accept connections
+        // Accepta conexiones
         client = accept(sock,(struct sockaddr*)&from,&fromlen);
         printf("Client connected\r\n");
 
-        // create our recv_cmds thread and parse client socket as a parameter
+        // Crea un recv_cmds thread y envia la informacion recibida como parametro
         CreateThread(NULL, 0,receive_cmds,(LPVOID)client, 0, &thread);
     }
 
-    // shutdown winsock
-    closesocket(sock);
-    WSACleanup();
-
-    // exit
-    return 0;
 }
